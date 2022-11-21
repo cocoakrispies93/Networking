f16c4fea3d55fe08497682a9755ff47cc71dcd55
cocoakrispies93 <89491635+cocoakrispies93@users.noreply.github.com>
2022-11-21 18:52:56 -0500

    Add files via upload

--- pingerUDPClient.py
+++ pingerUDPClient.py
@@ -1,41 +1,9 @@
-﻿# UDPPingerClient.py
-from socket import *
-#import socket
-import sys 	#In order to terminate the program 
-import time
-
+﻿from socket import *
 serverName = '10.0.2.5' #or '10.0.2.4' for vm copy
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-clientSocket.bind(('localhost', serverPort))
-server_addr = ('localhost', 12000)
-clientSocket.settimeout(1)
-
-try:
-    for i in range(1, 11):
-        start = time.time()
-        message = 'Ping #' + str(i) + " " + time.ctime(start)
-        try:
-            sent = clientSocket.sendto(bytes(message, 'UTF-8'), server_addr) 
-            #sent = clientSocket.sendto(bytes(message, 'UTF-8'), (serverName, serverPort)) #message.encode()
-            print("Sent " + message)
-            data, server = clientSocket.recvfrom(1024)
-            print("Received: " + str(data))
-            end = time.time();
-            elapsed = end - start
-            print("RTT: " + str(elapsed) + " seconds\n")
-        except timeout:
-            print("#" + str(i) + " Requested Time out\n")
-
-finally:
-    print("closing socket")
-    clientSocket.close()
-
-#while True: 
-#    message = input('Input ping message: ')
-#    clientSocket.sendto(message.encode(),(serverName, serverPort))
-#    message, address = clientSocket.recvfrom(1024)
-#    print(message.decode(), address)
-
+message = input('Input ping message: ')
+clientSocket.sendto(message.encode(),(serverName, serverPort))
+message, address = clientSocket.recvfrom(1024)
+print(message.decode())
 clientSocket.close()
-sys.exit()
--- pingerUDPServer.py
+++ pingerUDPServer.py
@@ -1,24 +1,9 @@
-# UDPPingerServer.py 
-# We will need the following module to generate randomized lost packets import random 
-from socket import * 
-import random
-# Create a UDP socket 
-# Notice the use of SOCK_DGRAM for UDP packets 
-serverSocket = socket(AF_INET, SOCK_DGRAM) 
-serverPort = 12000
-# Assign IP address and port number to socket 
-serverSocket.bind(('localhost', serverPort)) 
+﻿from socket import *
+serverPort = 49580
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
-while True: 
-    # Generate random number in the range of 0 to 10 
-    rand = random.randint(0, 10) 
-    # Receive the client packet along with the address it is coming from 
-    message, address = serverSocket.recvfrom(2048) 
-    # Capitalize the message from the client 
-    message = message.upper()
-    # If rand is less is than 4, we consider the packet lost and do not respond 
-    if rand < 4: 
-        continue 
-    # Otherwise, the server responds 
-    serverSocket.sendto(message, address)
-
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.decode().upper()
+    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
